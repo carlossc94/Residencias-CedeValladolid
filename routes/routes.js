6a0df4285511054dc6e922be8a0ef69450506174
Carlos Soberanis <carlos.soberanisc@outlook.com>
2016-11-23 12:56:42 -0600

    Autenticacion y Proteccion de Enlaces

--- routes/routes.js
+++ routes/routes.js
@@ -3,16 +3,24 @@ var router = express.Router();
 var passport =require('passport');
 var controllers=require ('../controllers');
 
+//middleware
+var authmiddle=require('../middleware/auth');
+
 router.get('/',controllers.HomeController.index);
 //Rutas de Usuario
 router.get('/auth/signup',controllers.UserController.SignUp);
 router.post('/auth/signup',controllers.UserController.PostSignUp);
 router.get('/auth/signin',controllers.UserController.getSignIn);
 
+router.get('/auth/logout',controllers.UserController.logout);
+router.get('/auth/userpanel', authmiddle.isLogged, controllers.UserController.getUserPanel);
+
 //autenticacion por el metodo local, la ruta de que salga bien el autenticacion, de que salga mal y activar un messaje flash en true
 router.post('/auth/signin', passport.authenticate('local',{
-	successRedirect : '/',
+	successRedirect : '/auth/userpanel',
 	failureRedirect: '/auth/signin',
 	failureFlash: true
 }));
+
+
 module.exports = router;
